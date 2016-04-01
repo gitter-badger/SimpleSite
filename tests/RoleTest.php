20bf499d5bb1411381af07ed3b72c989e1b8d2de
butschster <butschster@gmail.com>
2016-04-01 18:04:57 +0300

    Добавлены тесты для ролей

---
+++ tests/RoleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class RoleTest extends TestCase
+{
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRoleCreation()
+    {
+        /** @var App\Role $role */
+        $role = factory(App\Role::class)->create();
+        $this->assertTrue($role->exists);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testDefaultRolesSeeds()
+    {
+        $this->assertNotNull(\App\Role::whereName('admin')->first());
+        $this->assertNotNull(\App\Role::whereName('manager')->first());
+    }
+}
