693ddadefc15a3ae1a80b8d191832731a0146900
butschster <butschster@gmail.com>
2016-04-18 12:59:36 +0300

    Добавлен favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     <title>@lang('core.title.portal')</title>
+
     {!! Assets::getCssList() !!}
 </head>
 <body>
